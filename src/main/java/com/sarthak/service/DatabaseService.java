08063a85717da696dc10c98c4ee5a0b7478f370b
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-17 22:53:40 +0530

    added the fields for the DatabaseService class

--- src/main/java/com/sarthak/service/DatabaseService.java
+++ src/main/java/com/sarthak/service/DatabaseService.java
@@ -1,4 +1,10 @@
 package com.sarthak.service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
+    private static final String URL = "jdbc:mysql://localhost:3306/School";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "admin";
+    private Connection connection;
 }
