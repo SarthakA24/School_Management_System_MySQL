f8d5a14ccdcb30b076d4b79b7d56f9478610b632
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-17 22:39:33 +0530

    added the fields for the Student class

--- src/main/java/com/sarthak/model/Student.java
+++ src/main/java/com/sarthak/model/Student.java
@@ -1,4 +1,12 @@
 package com.sarthak.model;
 
 public class Student {
+    private int rollNumber;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String pinCode;
+    private String guardianName;
+    private long contactNumber;
+    private long guardianContactNumber;
 }
