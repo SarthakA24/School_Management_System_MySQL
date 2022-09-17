b399d37b6ea4f78d08f9b1a637542ff72aa70ce0
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-17 23:04:15 +0530

    added the methods in the interface Repository

--- src/main/java/com/sarthak/repository/Repository.java
+++ src/main/java/com/sarthak/repository/Repository.java
@@ -1,4 +1,18 @@
 package com.sarthak.repository;
 
+import com.sarthak.model.Student;
+
+import java.sql.Connection;
+import java.util.List;
+
 public interface Repository {
+    void addStudentData(Connection connection, Student student);
+
+    List<Student> getAllStudentData(Connection connection);
+
+    void deleteStudentData(Connection connection, int rollNumber);
+
+    Student searchStudentByRollNumber(Connection connection, int rollNumber);
+
+    void deleteStudent(Connection connection, int rollNumber);
 }
