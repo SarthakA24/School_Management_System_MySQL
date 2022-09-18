5bae8545a40e547bec9c67713c431ffca9d2fe26
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-18 13:15:48 +0530

    connected to the database and retrieved the connection object in a try catch block

--- src/main/java/com/sarthak/Main.java
+++ src/main/java/com/sarthak/Main.java
@@ -4,10 +4,19 @@ import com.sarthak.model.Student;
 import com.sarthak.repository.StudentRepository;
 import com.sarthak.service.DatabaseService;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         DatabaseService databaseService = new DatabaseService();
         StudentRepository studentRepository = new StudentRepository();
         Student student = new Student();
+        try {
+            databaseService.connect();
+            Connection connection = databaseService.getConnection();
+        } catch (SQLException e) {
+            System.err.println("!!Database Connection Failed!!");
+        }
     }
 }
