3b40d6d565b2bcf22a3de24b3e07c947bb29f3e7
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-18 13:22:38 +0530

    added the print statements for the menu

--- src/main/java/com/sarthak/service/DisplayService.java
+++ src/main/java/com/sarthak/service/DisplayService.java
@@ -4,6 +4,11 @@ public class DisplayService {
     public void displayMenu() {
         System.out.println("=========================");
         System.out.println("Welcome to the School!!");
+        System.out.println("Please select the operation you need to perform - ");
+        System.out.println("1. Add a student to the school.");
+        System.out.println("2. Display all the students in the school.");
+        System.out.println("3. Find a student's details.");
+        System.out.println("4. Remove a student from the school.");
         System.out.println("=========================");
     }
 }
