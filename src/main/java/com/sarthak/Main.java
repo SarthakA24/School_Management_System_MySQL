9fec812bb15c81beec40a67842b202a6ef299b4b
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-18 13:52:50 +0530

    removed the redundant initializer for choice

--- src/main/java/com/sarthak/Main.java
+++ src/main/java/com/sarthak/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         DatabaseService databaseService = new DatabaseService();
         StudentRepository studentRepository = new StudentRepository();
-        int choice = 0;
+        int choice;
         do {
             try {
                 Connection connection = databaseService.getConnection();
