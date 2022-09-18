38d14fd1a2fbff8cf3f800b32e057814c27602ee
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-18 13:13:47 +0530

    created objects for database service, student and student repository

--- src/main/java/com/sarthak/Main.java
+++ src/main/java/com/sarthak/Main.java
@@ -1,7 +1,13 @@
 package com.sarthak;
 
+import com.sarthak.model.Student;
+import com.sarthak.repository.StudentRepository;
+import com.sarthak.service.DatabaseService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseService databaseService = new DatabaseService();
+        StudentRepository studentRepository = new StudentRepository();
+        Student student = new Student();
     }
 }
